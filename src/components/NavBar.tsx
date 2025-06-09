9f405f7b7064506e7848650d49420c8ca2de1cc6
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2025-06-09 14:35:58 +0530

    Resolved the issue with tailwind css setup
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,15 +5,14 @@ import dropdown_icon from "../assets/Dropdown_icon.png";
 const NavBar = () => {
 
   return (
-    <nav className="flex justify-between items-center h-auto mb-[1rem] bg-[#FFFFFF] px-[3%] text-black">
+    <nav className="flex justify-between items-center h-auto my-4 bg-[#FFFFFF] px-[3%] text-black">
       <img src={truckpass_title} alt="truckpassTitle" className="h-[20px]" />
       <div className="flex space-x-[27px] items-center text-[15px]">
-        <p id='home' className='cursor-pointer font-semibold'>Home</p>
-        <p id='help' className='cursor-pointer font-semibold'>Help</p>
-
+        <p id='home' className='cursor-pointer font-[400]'>Home</p>
+        <p id='help' className='cursor-pointer font-[400]'>Help</p>
         <div className="flex gap-x-[4%] items-center">
           <img src={globe_icon} alt="truckpassTitle" className="h-[18px]" />
-          <p id='help' className='cursor-pointer font-semibold'>English</p>
+          <p id='help' className='cursor-pointer font-[400]'>English</p>
           <img src={dropdown_icon} alt="truckpassTitle" className="h-[6px]" />
         </div>
       
--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 
-const MainLayout: React.FC<MainLayoutProps> = ({children}) => {
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
     <div className="h-full w-full font-inter">
-        <NavBar />
-        {children}
+      <NavBar />
+      {children}
+      <footer className='text-sm text-[#717171] place-self-center pb-5'>
+        © 2025 TruckPass. All rights reserved.
+      </footer>
     </div>
   )
 }
 
 interface MainLayoutProps {
-    children: any;
+  children: any;
 }
 
 export default MainLayout
